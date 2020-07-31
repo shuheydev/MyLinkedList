10bf39ef031f8417fb669ed16f5b213eb445321c
shuheydev <shuhey.dev@gmail.com>
2020-07-31 18:07:49 +0900

    Add project files.

---
+++ MyLinkedList/MyLinkedList.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+
+namespace MyLinkedList
+{
+    public class MyLinkedList<T> : IEnumerable<T>
+    {
+        public MyLinkedListNode<T>? First => this._head.next == this._tail ? null : this._head.next;
+        public MyLinkedListNode<T>? Last => this._tail.previous == this._head ? null : this._tail.previous;
+
+        #region LinkedListの先頭と末尾のダミーノード
+        //リスト本体の要素ではない.
+        private MyLinkedListNode<T> _head { get; set; }
+        private MyLinkedListNode<T> _tail { get; set; }
+        #endregion
+
+        public int Count { get; private set; }
+        public MyLinkedList()
+        {
+            this._head = new MyLinkedListNode<T>();
+            this._tail = new MyLinkedListNode<T>();
+
+            Connect(this._head, this._tail);
+        }
+
+        public void AddLast(T value)
+        {
+            var node = new MyLinkedListNode<T>(value);
+            Insert(this._tail.previous, this._tail, node);
+            this.Count++;
+        }
+
+        public void AddFirst(T value)
+        {
+            var node = new MyLinkedListNode<T>(value);
+            Insert(this._head, this._head.next, node);
+            this.Count++;
+        }
+
+        public MyLinkedListNode<T> Find(T value)
+        {
+            for (var node = this._head.next; node != this._tail; node = node.next)
+            {
+                if (node.item.Equals(value))
+                {
+                    return node;
+                }
+            }
+
+            return null;
+        }
+
+        public void Remove(MyLinkedListNode<T> targetNode)
+        {
+            for (var node = this._head.next; node != this._tail; node = node.next)
+            {
+                if (node==targetNode)
+                {
+                    RemoveAt(node);
+                    Count--;
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var node = this._head.next; node != this._tail; node = node.next)
+            {
+                yield return node.item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+
+        private static void Connect(MyLinkedListNode<T> node1, MyLinkedListNode<T> node2)
+        {
+            node1.next = node2;
+            node2.previous = node1;
+        }
+
+        private static void Insert(MyLinkedListNode<T> node1, MyLinkedListNode<T> node2, MyLinkedListNode<T> newNode)
+        {
+            Connect(node1, newNode);
+            Connect(newNode, node2);
+        }
+
+        private static void RemoveAt(MyLinkedListNode<T> node)
+        {
+            Connect(node.previous, node.next);
+        }
+    }
+
+    public class MyLinkedListNode<T>
+    {
+        internal T item;
+        internal MyLinkedListNode<T>? next = null;
+        internal MyLinkedListNode<T>? previous = null;
+
+        public T Item => item;
+        public MyLinkedListNode<T>? Next => next;
+        public MyLinkedListNode<T>? Previous => previous;
+
+        public MyLinkedListNode()
+        {
+
+        }
+        public MyLinkedListNode(T item)
+        {
+            this.item = item;
+        }
+    }
+}
---
+++ MyLinkedListTests/MyLinkedListTests.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyLinkedList.Tests
+{
+    class AssertExtension
+    {
+        public static void AreEqual<T>(IEnumerable<T> collection1, IEnumerable<T> collection2)
+        {
+            var list1 = collection1.ToList();
+            int index = 0;
+            foreach (var element in collection2)
+            {
+                Assert.AreEqual(list1[index++], element);
+            }
+        }
+    }
+    [TestClass()]
+    public class MyLinkedListTests
+    {
+
+        [TestMethod()]
+        public void Createできるよね()
+        {
+            var linkedList = new MyLinkedList<int>();
+            Assert.AreEqual(0, linkedList.Count);
+            Assert.IsNull(linkedList.First);
+            Assert.IsNull(linkedList.Last);
+        }
+
+        [TestMethod()]
+        public void AddLastできるよね()
+        {
+            var linkedList = new MyLinkedList<int>();
+            linkedList.AddLast(100);
+            Assert.AreEqual(1, linkedList.Count);
+            Assert.AreEqual(100, linkedList.Last.Item);
+            linkedList.AddLast(200);
+            Assert.AreEqual(2, linkedList.Count);
+            AssertExtension.AreEqual<int>(linkedList, new[] { 100, 200 });
+
+            linkedList.AddLast(300);
+            Assert.AreEqual(3, linkedList.Count);
+            AssertExtension.AreEqual<int>(linkedList, new[] { 100, 200, 300 });
+        }
+
+        [TestMethod()]
+        public void AddFirstできるよね()
+        {
+            var linkedList = new MyLinkedList<int>();
+            linkedList.AddFirst(100);
+            Assert.AreEqual(1, linkedList.Count);
+            linkedList.AddLast(200);
+            Assert.AreEqual(2, linkedList.Count);
+
+            linkedList.AddFirst(300);
+            AssertExtension.AreEqual<int>(linkedList, new[] { 300, 100, 200 });
+        }
+
+        [TestMethod()]
+        public void Findできるかな()
+        {
+            var linkedList = new MyLinkedList<int>();
+            linkedList.AddLast(100);
+            linkedList.AddLast(200);
+            linkedList.AddLast(300);
+
+            var node = linkedList.Find(100);
+            Assert.AreSame(node, linkedList.First);
+            Assert.AreSame(linkedList.Find(200), linkedList.First.Next);
+            Assert.AreSame(linkedList.Find(300), linkedList.Last);
+
+            Assert.IsNull(linkedList.Find(400));
+        }
+
+        [TestMethod()]
+        public void Removeできるかな()
+        {
+            var linkedList = new MyLinkedList<int>();
+            linkedList.AddLast(100);
+            linkedList.AddLast(200);
+            linkedList.AddLast(300);
+            linkedList.AddLast(400);
+            linkedList.AddLast(500);
+
+            linkedList.Remove(linkedList.First);
+            AssertExtension.AreEqual(linkedList, new[] { 200, 300, 400, 500 });
+            Assert.AreEqual(4, linkedList.Count);
+            linkedList.Remove(linkedList.First.Next);
+            AssertExtension.AreEqual(linkedList, new[] { 200, 400, 500 });
+            Assert.AreEqual(3, linkedList.Count);
+            linkedList.Remove(linkedList.Last);
+            AssertExtension.AreEqual(linkedList, new[] { 200, 400 });
+            Assert.AreEqual(2, linkedList.Count);
+        }
+    }
+
+
+}
